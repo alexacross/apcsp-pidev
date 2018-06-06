d75c61839228fd33f251d3d404d32c0a95eecf00
Alexa Cross <alexa.acc3@gmail.com>
2018-06-06 16:44:28 +0000

    computerguess2

--- finalproject/computerguess.c
+++ finalproject/computerguess.c
@@ -2,10 +2,32 @@
 
 int main(void)
 {
+	int i;
 	int input;
-	printf("What is your secret number from 1-24?\n");
+	int guess;
+	printf("Choose a secret number from the following list: 1, 3, 6, 7, 12, 14, 15, 16, 25, 33, 34, 35, 37, 42, 43, 46, 48, or 49\n");
 	scanf("%d",&input);
-	printf("Your number is %d\n", input);
-;
+	int* ptrguess = &guess;
+	guess = 25;
+	if (input == 1 || input == 3)
+	{
+	  	 while ( input != guess )
+                        {
+                                if ( input < guess )
+                                {
+                                        *ptrguess = ( guess / 2 );
+                                }
+                                else if ( input > guess )
+                                {
+                                        *ptrguess = ( guess + ((50 - guess) / 2) );
+                                }
+                                printf("guess now equal to %d\n", guess);
+                        }
+		printf("The secret number is %d\n", guess);
+	}
+	else
+	{
+		printf("Please select a number from the list\n");
+	}
 }
 
